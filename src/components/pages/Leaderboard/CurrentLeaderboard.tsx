21aa5b15ec4d8a50290e075006721a48564524db
dans-temp <danielgoresht@gmail.com>
2026-01-03 15:00:02 -0700

    fix yimming pick

--- src/components/pages/Leaderboard/CurrentLeaderboard.tsx
+++ src/components/pages/Leaderboard/CurrentLeaderboard.tsx
@@ -4,7 +4,7 @@ import Leaderboard from './Leaderboard';
 const users = [
   { name: "Dan", stocks: ['WMT', 'CEG', 'NFLX', 'JNJ', 'PLTR'] },
   { name: "Lionel", stocks: ['K.TO', 'ARG.TO', 'LEGN', 'MOH', 'CVNA'] },
-  { name: "Yiming", stocks: ['MVST', 'NEXT', 'WELL', 'LULU', 'RGC'] },
+  { name: "Yiming", stocks: ['MVST', 'NEXT', 'WELL.TO', 'LULU', 'RGC'] },
   { name: "Raghav", stocks: ['NFLX', 'ORCL', 'NVO', 'T.TO', 'DJT'] },
   // { name: "Matt", stocks: ['INTC', 'OPEN', 'ICE', 'RZLV', 'MARA'] },
   { name: "Liyang", stocks: ['WDC', 'TEM', 'HWM', 'SHOP.TO', 'GLOB'] },
